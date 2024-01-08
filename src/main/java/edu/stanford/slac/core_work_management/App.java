be403c00d3b85e8413ec987a2f33240620fb313a
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-01-08 12:51:47 -0800

    first commit

---
+++ src/main/java/edu/stanford/slac/core_work_management/App.java
@@ -0,0 +1,11 @@
+package edu.stanford.slac.core_work_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
