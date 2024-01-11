f29089b0710fc963cf6b89ab78a1af73c49e6b81
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-01-10 18:00:40 -0800

    add openapi generation for cis service

--- src/main/java/edu/stanford/slac/core_work_management/service/WorkService.java
+++ src/main/java/edu/stanford/slac/core_work_management/service/WorkService.java
@@ -1,9 +1,14 @@
 package edu.stanford.slac.core_work_management.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@AllArgsConstructor
 public class WorkService {
+    // dedicated rest template for service to service communication
+    private final RestTemplate serviceRestTemplate;
     public void createNew() {
 
     }
