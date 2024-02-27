b87178e9c29c1ce8c64df99cfc1d8180dd7062b3
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-02-26 19:00:47 -0800

    start codin ghte new rest api

--- src/main/java/edu/stanford/slac/core_work_management/api/v1/controller/WorkController.java
+++ src/main/java/edu/stanford/slac/core_work_management/api/v1/controller/WorkController.java
@@ -19,7 +19,32 @@ package edu.stanford.slac.core_work_management.api.v1.controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.stanford.slac.ad.eed.baselib.api.v1.dto.ApiResultResponse;
+import edu.stanford.slac.ad.eed.baselib.api.v1.dto.AuthorizationTypeDTO;
+import edu.stanford.slac.ad.eed.baselib.exception.NotAuthorized;
+import edu.stanford.slac.core_work_management.api.v1.dto.ActivityDTO;
+import edu.stanford.slac.core_work_management.api.v1.dto.NewActivityDTO;
+import edu.stanford.slac.core_work_management.api.v1.dto.NewWorkDTO;
+import edu.stanford.slac.core_work_management.api.v1.dto.WorkDTO;
+import edu.stanford.slac.core_work_management.model.Work;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static edu.stanford.slac.ad.eed.baselib.exception.Utility.any;
+import static edu.stanford.slac.ad.eed.baselib.exception.Utility.assertion;
 
 /**
  * -----------------------------------------------------------------------------
@@ -32,7 +57,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
  * This file is part of core-work-management. It is subject to
  * the license terms in the LICENSE.txt file found in the top-level directory
  * of this distribution and at:
- * https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * <a href="https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html"/>.
  * No part of core-work-management, including this file, may be
  * copied, modified, propagated, or distributed except according to the terms
  * contained in the LICENSE.txt file.
@@ -42,9 +67,81 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Schema(description = "")
+@Schema(description = "/v1/work")
 public class WorkController {
 
-    // Add your service methods and variables here
+    @Operation(summary = "Create a new work")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Work saved")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ApiResultResponse<WorkDTO> createNew(
+            Authentication authentication,
+            @Valid @RequestBody NewWorkDTO newWorkDTO) {
+        return ApiResultResponse.of(null);
+    }
 
+    @Operation(summary = "Get work by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Work found"),
+            @ApiResponse(responseCode = "404", description = "Work not found")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/{workId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResultResponse<WorkDTO> findById(
+            Authentication authentication,
+            @Parameter(description = "Is the id of the work to find", required = true)
+            @PathVariable("workId") String id
+    ) {
+        return ApiResultResponse.of(null);
+    }
+
+    @Operation(summary = "Create a new work activity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Work saved")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(
+            path = "/{workId}/activity",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ApiResultResponse<ActivityDTO> createNew(
+            Authentication authentication,
+            @Parameter(description = "Is the work id for wich needs to be creates the activity", required = true)
+            @PathVariable("workId") String workId,
+            @Parameter(description = "The new activity to create", required = true)
+            @Valid @RequestBody NewActivityDTO newActivityDTO) {
+        return ApiResultResponse.of(null);
+    }
+
+    @Operation(summary = "find all element that respect the criteria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search operation completed successfully")
+    })
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ApiResultResponse<List<WorkDTO>> findAllElements(
+            Authentication authentication,
+            @Parameter(name = "anchorId", description = "Is the id of an entry from where start the search")
+            @RequestParam("anchorId") Optional<String> anchorId,
+            @Parameter(name = "contextSize", description = "Include this number of entries before the startDate (used for highlighting entries)")
+            @RequestParam("contextSize") Optional<Integer> contextSize,
+            @Parameter(name = "limit", description = "Limit the number the number of entries after the start date.")
+            @RequestParam(value = "limit") Optional<Integer> limit,
+            @Parameter(name = "search", description = "Typical search functionality")
+            @RequestParam("search") Optional<String> search,
+            @Parameter(name = "tags", description = "Only include entries that use one of these tags")
+            @RequestParam("tags") Optional<List<String>> tags,
+            @Parameter(name = "requireAllTags", description = "Require that all entries found includes all the tags")
+            @RequestParam(value = "requireAllTags", defaultValue = "false") Optional<Boolean> requireAllTags
+    ) {
+        return ApiResultResponse.of(
+                Collections.emptyList()
+        );
+    }
 }
