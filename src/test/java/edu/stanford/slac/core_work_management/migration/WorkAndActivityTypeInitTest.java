3edca2364844b25a760ceba56d0887c6c4d318e5
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-03-05 17:19:00 -0800

    add test for setup work and activity type

---
+++ src/test/java/edu/stanford/slac/core_work_management/migration/WorkAndActivityTypeInitTest.java
@@ -0,0 +1,62 @@
+package edu.stanford.slac.core_work_management.migration;
+
+import edu.stanford.slac.core_work_management.model.*;
+import edu.stanford.slac.core_work_management.repository.ActivityRepository;
+import edu.stanford.slac.core_work_management.repository.ActivityTypeRepository;
+import edu.stanford.slac.core_work_management.repository.WorkTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@AutoConfigureMockMvc
+@SpringBootTest()
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles({"test"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class WorkAndActivityTypeInitTest {
+    @Autowired
+    private WorkTypeRepository workTypeRepository;
+    @Autowired
+    private ActivityTypeRepository activityTypeRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    public void cleanCollection() {
+        mongoTemplate.remove(new Query(), WorkType.class);
+        mongoTemplate.remove(new Query(), ActivityType.class);
+    }
+
+    @Test
+    public void initTest() {
+        InitWorkAndActivityType initWorkAndActivityType = new InitWorkAndActivityType(workTypeRepository, activityTypeRepository);
+        assertDoesNotThrow(initWorkAndActivityType::changeSet);
+        assertThat(workTypeRepository.findAll())
+                .isNotEmpty()
+                .extracting(WorkType::getTitle).containsExactlyInAnyOrder(
+                        "hw maintenance",
+                        "hw installation / removal",
+                        "hw problem",
+                        "sw maintenance",
+                        "sw installation/ removal",
+                        "sw problem",
+                        "project enhancement request",
+                        "administrative activity"
+                );
+    }
+}
