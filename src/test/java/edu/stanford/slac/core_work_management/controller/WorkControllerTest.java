f459bb46fe91b5fa1cee389568a78e74f15f0e78
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-03-05 16:49:15 -0800

    fixes

--- src/test/java/edu/stanford/slac/core_work_management/controller/WorkControllerTest.java
+++ src/test/java/edu/stanford/slac/core_work_management/controller/WorkControllerTest.java
@@ -450,6 +450,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -491,6 +492,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -532,6 +534,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -573,6 +576,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.get(2))
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -615,6 +619,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -852,6 +857,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
@@ -934,6 +940,7 @@ public class WorkControllerTest {
                                         .activityTypeId(testActivityTypeIds.getFirst())
                                         .title("New activity 1")
                                         .description("activity 1 description")
+                                        .activityTypeSubtype(ActivityTypeSubtypeDTO.Other)
                                         .build()
                         )
                 );
