ff41648800c12886bf04f4777520a6da7e08e564
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-03-20 09:42:17 -0700

    add field tha hase been lefover

--- src/main/java/edu/stanford/slac/core_work_management/api/v1/dto/NewActivityDTO.java
+++ src/main/java/edu/stanford/slac/core_work_management/api/v1/dto/NewActivityDTO.java
@@ -21,6 +21,18 @@ public record NewActivityDTO (
         @NotEmpty
         @Schema(description = "The detailed description of the activity")
         String description,
+        @Schema(description = "The description of the test plan for the activity. This field provides a detailed description of the test plan associated with the activity.")
+        String testPlanDescription,
+        @Schema(description = "The description of the backout plan for the activity. This field provides a detailed description of the backout plan associated with the activity.")
+        String backoutPlanDescription,
+        @Schema(description = "The description of the system requirements for the activity. This field provides a detailed description of the system requirements associated with the activity.")
+        String systemRequiredDescription,
+        @Schema(description = "The description of the system effects for the activity. This field provides a detailed description of the system effects associated with the activity.")
+        String systemEffectedDescription,
+        @Schema(description = "The description of the risk and benefits for the activity. This field provides a detailed description of the risk and benefits associated with the activity.")
+        String riskBenefitDescription,
+        @Schema(description = "The description of the dependencies for the activity. This field provides a detailed description of the dependencies associated with the activity.")
+        String dependenciesDescription,
         @NotEmpty
         @Schema(description = "The type of the activity expressed by it's identifier")
         String activityTypeId,
