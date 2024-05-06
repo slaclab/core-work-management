62428a845d35b5681b35b75ce8c05adf15e51ccb
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-05-06 08:53:34 -0700

    add init for the domain and moved all the initilization of data to use the profile init-default-data

---
+++ src/test/java/edu/stanford/slac/core_work_management/migration/DomainInitTest.java
@@ -0,0 +1,65 @@
+package edu.stanford.slac.core_work_management.migration;
+
+import edu.stanford.slac.core_work_management.api.v1.dto.DomainDTO;
+import edu.stanford.slac.core_work_management.api.v1.dto.WATypeCustomFieldDTO;
+import edu.stanford.slac.core_work_management.api.v1.dto.WorkTypeDTO;
+import edu.stanford.slac.core_work_management.model.Domain;
+import edu.stanford.slac.core_work_management.model.LOVElement;
+import edu.stanford.slac.core_work_management.model.WorkType;
+import edu.stanford.slac.core_work_management.repository.WorkTypeRepository;
+import edu.stanford.slac.core_work_management.service.DomainService;
+import edu.stanford.slac.core_work_management.service.LOVService;
+import edu.stanford.slac.core_work_management.service.WorkService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@AutoConfigureMockMvc
+@SpringBootTest()
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles({"test"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class DomainInitTest {
+    @Autowired
+    private DomainService domainService;
+
+    @Autowired
+    private WorkTypeRepository workTypeRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    public void cleanCollection() {
+        mongoTemplate.remove(new Query(), Domain.class);
+    }
+
+    @Test
+    public void initTest() {
+        InitDomain initWorkType = new InitDomain(domainService);
+        assertDoesNotThrow(initWorkType::changeSet);
+        var allDomain = assertDoesNotThrow(
+                () -> domainService.finAll()
+        );
+        assertThat(allDomain).isNotNull();
+        assertThat(allDomain.size()).isEqualTo(1);
+        assertThat(allDomain)
+                .extracting(DomainDTO::name)
+                .contains("tec");
+        assertThat(allDomain)
+                .extracting(DomainDTO::description)
+                .contains("TEC");
+    }
+}
