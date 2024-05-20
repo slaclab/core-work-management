d2c070c3304d5932b71862a9b5ba279575f81fc1
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-05-20 14:08:57 -0700

    log on staartup the uath on elog topics

--- src/main/java/edu/stanford/slac/core_work_management/config/KafkaConfig.java
+++ src/main/java/edu/stanford/slac/core_work_management/config/KafkaConfig.java
@@ -3,18 +3,18 @@ package edu.stanford.slac.core_work_management.config;
 import edu.stanford.slac.core_work_management.elog_api.dto.ImportEntryDTO;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.MicrometerProducerListener;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.Map;
 
+@Log4j2
 @Configuration
 @RequiredArgsConstructor
 @AutoConfigureBefore(KafkaAutoConfiguration.class)
@@ -22,6 +22,29 @@ public class KafkaConfig {
     private final MeterRegistry meterRegistry;
     private final KafkaProperties kafkaProperties;
 
+    @Value("${edu.stanford.slac.core-work-management.elog-import-topic}")
+    private String importEntryTopic;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        StringBuilder topicDescriptionLog = new StringBuilder();
+        Map<String, Object> configs = kafkaProperties.buildAdminProperties();
+        var kAdmin = new KafkaAdmin(configs);
+        var topicDescription = kAdmin.describeTopics(importEntryTopic);
+        if (topicDescription.containsKey(importEntryTopic)) {
+            if(topicDescription.get(importEntryTopic).authorizedOperations() != null) {
+                topicDescription.get(importEntryTopic).authorizedOperations().forEach(
+                        authorizedOperation -> topicDescriptionLog.append("Topic %s has authorized operation %s".formatted(importEntryTopic, authorizedOperation.toString()))
+                );
+            } else {
+                topicDescriptionLog.append("Topic %s has no authorized operations".formatted(importEntryTopic));
+            }
+        } else {
+            topicDescriptionLog.append("Topic %s is not in the broker".formatted(importEntryTopic));
+        }
+        log.info(topicDescriptionLog.toString());
+        return kAdmin;
+    }
 
     @Bean
     public ProducerFactory<String, ImportEntryDTO> importEntryProducerFactory() {
