198d24fc37644246438f7cd7cfc3fa40c71fdb14
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-06-17 18:48:37 -0700

    clean code

--- src/main/java/edu/stanford/slac/core_work_management/model/BucketSlot.java
+++ src/main/java/edu/stanford/slac/core_work_management/model/BucketSlot.java
@@ -1,11 +1,6 @@
 package edu.stanford.slac.core_work_management.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import edu.stanford.slac.core_work_management.model.value.LOVField;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.data.annotation.*;
 
